0cda58f04a5839f5b6cdcfda15db083a825c60a9
anna brezhneva <brezhneva.a@yandex.ru>
2022-12-14 20:53:03 +0300

    Решение задачи 62 Добавила вывод матрицы ровным квадратом

--- 62_zadacha/Program.cs
+++ 62_zadacha/Program.cs
@@ -36,7 +36,14 @@ void PrintArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Write($"{array[i, j]} ");
+            if (array[i,j] / 10 <= 0)
+            {
+                Write($"0{array[i, j]} ");
+            }
+            else
+            {
+                Write($"{array[i, j]} ");
+            }
         }
         WriteLine();
     }
